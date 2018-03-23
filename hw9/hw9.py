62f728b7746bcd09df8f9caec6e127f65f00be7a
Sonia Goldina <31884568+troubledmuffin@users.noreply.github.com>
2018-03-23 21:48:27 +0300

    Update hw9.py

--- hw9/hw9.py
+++ hw9/hw9.py
@@ -3,7 +3,7 @@ fname=input('Enter file name: ')
 def regfind():
     with open (fname, encoding = 'utf-8') as f:
         text = f.read()
-    a = re.findall('[Зз]агру[жз][иуяе][в]?[мтшлвн]?[иеа]?[ямйгуьенсо]?[усоыхю]?[ясагм]?[яьеихюйоу]?', text)
+    a = re.findall(r'\b[Зз]агру[жз][иуяе][в]?[мтшлвн]?[иеа]?[ямйгуьенсо]?[усоыхю]?[ясагм]?[яьеихюйоу]?\b', text)
     print(a)
     
 print(regfind())
