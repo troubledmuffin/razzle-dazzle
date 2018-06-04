c44a47bf47f9274cbc8c1da4ae14d2c256737100
Sonia Goldina <31884568+troubledmuffin@users.noreply.github.com>
2018-06-04 23:39:55 +0300

    Update hw14.py

--- hw14.py
+++ hw14.py
@@ -6,20 +6,17 @@ text = text.replace('\xa0', '')
 phrases  = text.split('. ')
 print (phrases)
 for phrase in phrases:
-    b = []
     symbols = """(-),:;"'—»«"""
     for s in symbols:
         phrase = phrase.replace(s, '')
     phrase = phrase.split()
-    for i in phrase:
-        b.append(len(i))
+    b = [len(i) for i in phrase]
     a = sum(b)
     avrg = a / len(phrase)
     if len(phrase) > 10:
         print('Это предложение со словами длины {:01.2f}'.format(avrg))
     else:
         print('А это слишком короткое предложение!')
-
     
     
 
